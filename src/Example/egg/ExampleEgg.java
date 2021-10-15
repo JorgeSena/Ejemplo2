b7ee66096cd64153ad8087eb01b934be9705caf5
Jorge Seña <jorge_gllen@hotmail.com>
2021-10-15 20:28:41 +0000

    Primer Commit

---
+++ src/Example/egg/ExampleEgg.java
@@ -0,0 +1,12 @@
+
+package example.egg;
+
+
+public class ExampleEgg {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/Example/egg/entity/Mascota.java
@@ -0,0 +1,34 @@
+
+package Example.egg.entity;
+
+
+public class Mascota {
+    private Integer id;
+    private String nombre;
+
+    public Mascota() {
+    }
+
+    public Mascota(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
